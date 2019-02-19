0a53ce2deb2d1a5d6cb0d1fa36351270c8628ca6
cz_jjq <cz_jjq@163.com>
2019-02-19 11:25:26 +0800

    upgrade

--- src/driver/.secbulk.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/jello/Downloads/dnw-linux/src/driver/secbulk.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-67-generic/scripts/module-common.lds --build-id  -o /home/jello/Downloads/dnw-linux/src/driver/secbulk.ko /home/jello/Downloads/dnw-linux/src/driver/secbulk.o /home/jello/Downloads/dnw-linux/src/driver/secbulk.mod.o
--- src/driver/.tmp_versions/secbulk.mod
+++
@@ -1,2 +0,0 @@
-/home/jello/Downloads/dnw-linux/src/driver/secbulk.ko
-/home/jello/Downloads/dnw-linux/src/driver/secbulk.o
